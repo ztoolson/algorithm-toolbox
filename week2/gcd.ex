53391c03452b86d1833b2e2902e6892f30742e6d
Zach Toolson <zach.toolson@mx.com>
2016-09-20 08:57:30 -0600

    add comment to GCD with the problem

--- week2/gcd.ex
+++ week2/gcd.ex
@@ -16,7 +16,19 @@ defmodule GCDTest do
   end
 end
 
+# Problem Description
+# Task:
+#   Given two integers a and b, find their greatest common divisor.
+# 
+# Input Format.
+#   The two integers a, b are given in the same line separated by space.
+# 
+# Constraints:
+#   1≤ a, b ≤ 2·10^9.
+# Output Format:
+#   OutputGCD(a, b)
 defmodule GCD do
+  # TODO: read from standard input and output to standard output the result
   def compute(a, b) do
     _gcd(a, b)
   end
