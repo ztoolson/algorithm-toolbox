6f840e996898c673cdde5e8c0e8fa30a3909742e
Zach Toolson <zach.toolson@mx.com>
2016-09-21 08:44:54 -0600

    remove naive gcd algorithm

--- week2/gcd.ex
+++ week2/gcd.ex
@@ -2,19 +2,6 @@ ExUnit.start(timeout: 2000) # 2 seconds
 
 defmodule GCDTest do
   use ExUnit.Case, async: true
-
-  test "gcd(10, 4)" do
-    assert GCD.compute(10, 4) == 2
-  end
-
-  test "gcd(18, 35)" do
-    assert GCD.compute(18, 35) == 1
-  end
-
-  test "gcd(28851538, 1183019)" do
-    assert GCD.compute(28851538, 1183019) == 17657
-  end
-
   test "euclidean gcd(10, 4)" do
     assert GCD.compute_euclidean(10, 4) == 2
   end
@@ -49,22 +36,6 @@ end
 #   OutputGCD(a, b)
 defmodule GCD do
   # TODO: read from standard input and output to standard output the result
-  def compute(a, b) do
-    _gcd(a, b)
-  end
-
-  defp _gcd(a, b, current_divisor \\ 1, gcd \\ 0)
-  defp _gcd(a, b, current_divisor, gcd) when current_divisor >= (a + b), do: gcd
-  defp _gcd(a, b, current_divisor, gcd) do
-    if rem(a, current_divisor) == 0 && rem(b, current_divisor) == 0 do
-      _gcd(a, b, current_divisor+1, current_divisor)
-    else
-      _gcd(a, b, current_divisor + 1, gcd)
-    end
-  end
-
-
-  # gcd(a, b) == gcd(rem(a, b), b) == gcd(b, rem(a, b)
   def compute_euclidean(a, b) do
     _euclid_gcd(a, b)
   end
